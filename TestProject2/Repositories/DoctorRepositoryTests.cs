8820023452c1d44ac5088b2a629347f10a03c2e4
James Lovesay <james.lovesay@ivendi.com>
2022-12-02 12:18:45 +0000

    add mock repository to test doctors repo.

--- TestProject2/Repositories/DoctorRepositoryTests.cs
+++ TestProject2/Repositories/DoctorRepositoryTests.cs
@@ -21,6 +21,68 @@ namespace HospitalManagementSystem.Api.Tests.Repositories
         {
             var mongoFactory = new Mock<IMongoFactory>();
             var runner = MongoDbRunner.Start();
+
+            MongoClient client = new MongoClient(runner.ConnectionString);
+            var database = client.GetDatabase("DoctorRepositoryTests");
+            mongoFactory.Setup(x => x.GetDatabase()).Returns(database);
+
+            _doctorCollection = database.GetCollection<DoctorReadModel>(nameof(DoctorReadModel));
+            _doctorCollection.InsertMany(new List<DoctorReadModel> {
+                new DoctorReadModel
+                {
+                    Name = "Dr Test A",
+                    Specialism = "Orthopaedics",
+                    Status = "ActivePermanent",
+                    HourlyChargingRate = 800,
+                    DoctorId = new MongoDB.Bson.ObjectId(),
+                    _id = new MongoDB.Bson.ObjectId(),
+                },
+                new DoctorReadModel
+                {
+                    Name = "Dr Test B",
+                    Specialism = "Psychiatry",
+                    Status = "ActiveTemporary",
+                    HourlyChargingRate = 500,
+                    DoctorId = new MongoDB.Bson.ObjectId(),
+                    _id = new MongoDB.Bson.ObjectId(),
+                },
+                new DoctorReadModel
+                {
+                    Name = "Dr Test C",
+                    Specialism = "Neurology",
+                    Status = "Inactive",
+                    HourlyChargingRate = 600,
+                    DoctorId = new MongoDB.Bson.ObjectId(),
+                    _id = new MongoDB.Bson.ObjectId(),
+                },
+                new DoctorReadModel
+                {
+                    Name = "Mr Test D",
+                    Specialism = "Psychology",
+                    Status = "ActivePermanent",
+                    HourlyChargingRate = 700,
+                    DoctorId = new MongoDB.Bson.ObjectId(),
+                    _id = new MongoDB.Bson.ObjectId(),
+                },
+                new DoctorReadModel
+                {
+                    Name = "Dr Test E",
+                    Specialism = "Urology",
+                    Status = "ActiveTemporary",
+                    HourlyChargingRate = 600,
+                    DoctorId = new MongoDB.Bson.ObjectId(),
+                    _id = new MongoDB.Bson.ObjectId(),
+                },
+                new DoctorReadModel
+                {
+                    Name = "Mr Test F",
+                    Specialism = "Orthopaedics",
+                    Status = "Inactive",
+                    HourlyChargingRate = 750,
+                    DoctorId = new MongoDB.Bson.ObjectId(),
+                    _id = new MongoDB.Bson.ObjectId(),
+                }
+            });
         }
     }
 }
